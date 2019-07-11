42fc2712561d94ce0ad6da9ec649b8f8179b163f
adesheddie <adesh.eddie@gmail.com>
2019-07-11 17:46:18 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+module.exports = function test(string) {
+    if (typeof string !== "string") throw new TypeError("Test wants a string!");
     return string.replace(/\s/g, "");
   };
